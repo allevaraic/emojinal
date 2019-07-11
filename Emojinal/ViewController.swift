08073b759814053abfa01d254180f9b067150152
Amber Ross <ross.amber1540@gmail.com>
2019-07-11 16:28:31 -0700

    Itiation 2

--- Emojinal/ViewController.swift
+++ Emojinal/ViewController.swift
@@ -9,7 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBAction func happyButton(_ sender: UIButton) {
+        let tojAlert =  UIAlertController (title: "Happy Movie", message: "Mrs. Doubtfire", preferredStyle: UIAlertController.Style.alert)
+        
+        tojAlert.addAction(UIAlertAction(title: "Okay", style: UIAlertAction.Style.default, handler: nil))
+        present(tojAlert, animated: true, completion: nil)
 
+    }
+    
+    @IBAction func sadButton(_ sender: UIButton) {
+        let tojAlert =  UIAlertController (title: "Sad Movie", message: "The Choice", preferredStyle: UIAlertController.Style.alert)
+        tojAlert.addAction(UIAlertAction(title: "Okay", style: UIAlertAction.Style.default, handler: nil))
+        present(tojAlert, animated: true, completion: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
