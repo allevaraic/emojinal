5cf1bf5a35f0b3f60d6b67cc967984c21abc21f7
Amber Ross <ross.amber1540@gmail.com>
2019-07-12 16:25:20 -0700

    Emojinal final save

--- Emojinal/ViewController.swift
+++ Emojinal/ViewController.swift
@@ -9,18 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBAction func happyButton(_ sender: UIButton) {
-        let tojAlert =  UIAlertController (title: "Happy Movie", message: "Mrs. Doubtfire", preferredStyle: UIAlertController.Style.alert)
-        
-        tojAlert.addAction(UIAlertAction(title: "Okay", style: UIAlertAction.Style.default, handler: nil))
-        present(tojAlert, animated: true, completion: nil)
+let emojis = ["🥺":"sad", "😃":"happy"]
+var movieOptions =
+    ["sad":["The Choice", "Les Misérables", "Good Will Hunting", "West Side Story", "6 Feet Apart", "The Notebook", "Titanic"],
+     "happy":["Legally Blonde", "Mrs. Doubtfire", "Mamma Mia", "Alladin", "Freaky Friday", "Breakfast At Tiffany's", "Ferris Buellar's Day Off"]
+]
 
+    @IBAction func happyButton(_ sender: UIButton) {
+        let selectedEmotion = sender.titleLabel?.text
+        movieOptions[emojis[selectedEmotion!]!]!.shuffle()
+        let emojiMessage =
+        movieOptions[emojis[selectedEmotion!]!]?[0]
+        let alert = UIAlertController(title:emojis["😃"]!, message: emojiMessage, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title:"Done", style: .default, handler: nil))
+        self.present(alert, animated: true)
+        //alert.addAction(UIAlertAction(title:"Bye", style: .default, handler: nil))
+        
+        
     }
     
     @IBAction func sadButton(_ sender: UIButton) {
-        let tojAlert =  UIAlertController (title: "Sad Movie", message: "The Choice", preferredStyle: UIAlertController.Style.alert)
-        tojAlert.addAction(UIAlertAction(title: "Okay", style: UIAlertAction.Style.default, handler: nil))
-        present(tojAlert, animated: true, completion: nil)
+        let selectedEmotion = sender.titleLabel?.text
+        movieOptions[emojis[selectedEmotion!]!]!.shuffle()
+        let emojiMessage =
+            movieOptions[emojis[selectedEmotion!]!]?[0]
+        let alert = UIAlertController(title:emojis["🥺"]!, message: emojiMessage, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title:"Done", style: .default, handler: nil))
+        self.present(alert, animated: true)
+       
+        
+        
+        //alert.addAction(UIAlertAction(title: "Bye", style: .default, handler: nil))
+        
+        
+        //let tojAlert =  UIAlertController (title: "Sad Movie", message: "The Choice", preferredStyle: UIAlertController.Style.alert)
+        //tojAlert.addAction(UIAlertAction(title: "Okay", style: UIAlertAction.Style.default, handler: nil))
+       // present(tojAlert, animated: true, completion: nil)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
